85df8cb668f8452394e2203a269da63388af32ce
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-09 19:32:13 +0530

    Update __main__.py

--- Alexia/__main__.py
+++ Alexia/__main__.py
@@ -879,7 +879,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    about_callback_handler = CallbackQueryHandler(Alexia_robot_about_callback, pattern=r"Alexia_robot_")
+    about_callback_handler = CallbackQueryHandler(alexia_robot_about_callback, pattern=r"alexia_robot_")
     source_callback_handler = CallbackQueryHandler(Source_about_callback, pattern=r"source_")
 
     donate_handler = CommandHandler("donate", donate)
