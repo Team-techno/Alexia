1c5fe3deadc09c9ff96ec689b7773fb740e33cfa
MichaelFaraday <88786146+Sumans11@users.noreply.github.com>
2022-01-10 12:10:54 +0530

    Update logomaker.py

--- Alexia/modules/logomaker.py
+++ Alexia/modules/logomaker.py
@@ -280,7 +280,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "Alexia.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Fʟᴏʀɪɴᴀ](https://t.me/Florina_robot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Alexia](https://t.me/Alexia_robot)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
