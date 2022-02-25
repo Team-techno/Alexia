4a6831faad52c75e516bfa10c34b71b21750297c
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-25 08:06:05 +0530

    Update misc.py

--- Alexia/modules/helper_funcs/misc.py
+++ Alexia/modules/helper_funcs/misc.py
@@ -68,7 +68,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2, 2]:
         pairs.append((modules[-1],))
     else:
-        pairs += [[EqInlineKeyboardButton("─※·𝗚𝗼 𝗛𝗼𝗺𝗲·※─", callback_data="florina_back")]]
+        pairs += [[EqInlineKeyboardButton("─※·𝗚𝗼 𝗛𝗼𝗺𝗲·※─", callback_data="alexia_back")]]
 
     return pairs
 
