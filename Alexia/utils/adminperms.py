becaf2879963a5223f90eb0510b8e946d383161a
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-27 12:14:53 +0530

    Update adminperms.py

--- Alexia/utils/adminperms.py
+++ Alexia/utils/adminperms.py
@@ -1,17 +1,28 @@
-from telegram import Chat, User
+import asyncio
+from pyrogram import filters
+from pyrogram.types import ChatPermissions, Message
+from Alexia import pbot as app
 
 
-def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_promote_members
-
-
-def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_restrict_members
-
-
-def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_pin_messages
-
-
-def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_change_info
+async def member_permissions(chat_id: int, user_id: int):
+    perms = []
+    member = await app.get_chat_member(chat_id, user_id)
+    if member.can_post_messages:
+        perms.append("can_post_messages")
+    if member.can_edit_messages:
+        perms.append("can_edit_messages")
+    if member.can_delete_messages:
+        perms.append("can_delete_messages")
+    if member.can_restrict_members:
+        perms.append("can_restrict_members")
+    if member.can_promote_members:
+        perms.append("can_promote_members")
+    if member.can_change_info:
+        perms.append("can_change_info")
+    if member.can_invite_users:
+        perms.append("can_invite_users")
+    if member.can_pin_messages:
+        perms.append("can_pin_messages")
+    if member.can_manage_voice_chats:
+        perms.append("can_manage_voice_chats")
+    return perms
