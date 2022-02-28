047e1f8b9822e01440702bd03aa4ebcfedc82512
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-28 05:58:57 +0530

    Update stickers.py

--- Alexia/modules/stickers.py
+++ Alexia/modules/stickers.py
@@ -525,7 +525,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./FlorinaRobot/resources/ArmWrestler.ttf"
+        fnt = "./Alexia/resources/ArmWrestler.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
